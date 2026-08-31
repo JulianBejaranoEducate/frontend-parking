dafe64494742329c6511bb697ee5251ea63480e8
Julian Bejarano <julian@nesshq.com>
2026-08-31 13:48:37 -0500

    fase [estructura inicial carpetas] - desarrollo frontend

---
+++ angular/parking/src/app/components/main-dashboard/main-dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-main-dashboard',
+  styleUrl: './main-dashboard.css',
+  templateUrl: './main-dashboard.html',
+})
+export class MainDashboard {}
---
+++ angular/parking/src/app/components/register-vehicle/register-vehicle.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-register-vehicle',
+  styleUrl: './register-vehicle.css',
+  templateUrl: './register-vehicle.html',
+})
+export class RegisterVehicle {}
